ac52624528cc5558e71e8b9fa25788ec3cd71302
trumpetfish1 <evan.perrygiblin@gmail.com>
2026-07-23 20:46:20 -0400

    React and onchain interactions

--- 09-dao/frontend/src/contract/abi.js
+++ 09-dao/frontend/src/contract/abi.js
@@ -1,8 +1,22 @@
+// src/contract/abi.js
+
 export const DAO_ABI = [
-  "function addMember(address)",
-  "function createProposal(string,address,uint256)",
-  "function vote(uint256)",
-  "function executeProposal(uint256)",
-  "function getProposal(uint256) view returns(string,address,uint256,bool,uint256)",
-  "function isMember(address) view returns(bool)"
+
+    // Members
+    "function addMember(address _member)",
+    "function members(address) view returns (bool)",
+
+    // Treasury
+    "function deposit() payable",
+
+    // Proposals
+    "function createProposal(string,address,uint256)",
+    "function vote(uint256)",
+    "function executeProposal(uint256)",
+
+    // Views
+    "function getProposal(uint256) view returns (string,address,uint256,bool,uint256)",
+
+    // Public proposal getter
+    "function proposals(uint256) view returns (string,address,uint256,bool,uint256)"
 ];
